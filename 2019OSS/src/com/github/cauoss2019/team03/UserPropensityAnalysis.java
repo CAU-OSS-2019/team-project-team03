5ba9a14d32f12a0613a2bf469a368c24f0070010
Hansol Cho <sorrowsorryr@gmail.com>
2019-05-16 23:19:51 +0900

    Console Menu
    TODO: Scanner Issue (Menu 클래스의 While문 안에서 Scanner Error 발생) "No Line Found" 해결하기

---
+++ 2019OSS/src/com/github/cauoss2019/team03/UserPropensityAnalysis.java
@@ -0,0 +1,13 @@
+package com.github.cauoss2019.team03;
+
+public class UserPropensityAnalysis {
+
+	public UserPropensityAnalysis() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+}
