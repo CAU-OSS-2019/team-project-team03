d157ee88245a36b82dffd4055353628c7f55722c
Jin <pyj1348@gmail.com>
2019-06-10 01:14:01 +0900

    Add basic book_recommender code
    Add basic book_recommender code

--- 2019OSS/src/com/github/cauoss2019/team03/Read.java
+++ 2019OSS/src/com/github/cauoss2019/team03/Read.java
@@ -1,5 +1,3 @@
-package com.github.cauoss2019.team03;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,43 +7,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Read {
-	
-	List<List<String>> records = new ArrayList<>();
-	
-	public Read() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Read(String path, String encoding) {
-		
-		BufferedReader br = null;
-		
-		try {
-			
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(path), encoding));
-			
-			String line;
-			
-			while((line = br.readLine()) != null) {
-					
-					String[] values = line.split(",");
-					records.add(Arrays.asList(values));
-				}
-	
-		} catch(Exception e) {
-			
-			e.printStackTrace();
-		}
 
-	
-	}
+    List<List<String>> records = new ArrayList<List<String>>();
 
-	public List<List<String>> getRecords() {
-		return records;
-	}
+    public Read() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	public void setRecords(List<List<String>> records) {
-		this.records = records;
-	}
+    public Read(String path, String encoding) {
+
+        BufferedReader br = null;
+
+        try {
+
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(path), encoding));
+
+            String line;
+
+            while((line = br.readLine()) != null) {
+
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public List<List<String>> getRecords() {
+        return records;
+    }
+
+    public void setRecords(List<List<String>> records) {
+        this.records = records;
+    }
 }
