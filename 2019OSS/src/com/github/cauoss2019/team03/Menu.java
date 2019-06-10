42db010722fa02a45f9e537fe8659212e9b8d4dd
criticaster <43430173+criticaster@users.noreply.github.com>
2019-06-10 18:14:53 +0900

    전체데이터에서 유저 ID별로 개인 데이터 추출 및 성향 분석 업데이트
    Main.java : 기존 고정 아이디에서 1~1000까지 유저 아이디를 받게 끔 수정
    Menu.java : print문 수정
    UserPropensityAnalysis.java : 전체 데이터에서 유저 ID별로 개인 데이터 추출 및 성향 분석으로 변경, 결과 print문 수정

--- 2019OSS/src/com/github/cauoss2019/team03/Menu.java
+++ 2019OSS/src/com/github/cauoss2019/team03/Menu.java
@@ -1,53 +1,56 @@
+package com.github.cauoss2019.team03;
+
 import java.util.Scanner;
 
 public class Menu {
+	
+	private int menuInput = 0; 
 
-    private int menuInput = 0;
+	public Menu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Menu(String InputID) {
+		
+		System.out.println(InputID + " 님 환영합니다 !\n\n");
+		String tempInput = null;
+		
+		do {
+			
+			
+			System.out.println("메뉴 번호를 입력해 주세요.\n");
+			
+			System.out.println("=========================");
+			
+			System.out.println("1. 사용자 성향 분석 목록\n");
+			System.out.println("2. 사용자 대출 이력/반납 목록\n");
+			System.out.println("3. 추천 시스템 이용\n");
+			System.out.println("4. 어플리케이션 종료\n");
 
-    public Menu() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+			System.out.print(">> ");
+			
+			@SuppressWarnings("resource")
+			Scanner sc = new Scanner(System.in);
+			
+			tempInput = sc.next();
+			
+			menuInput = Integer.parseInt(tempInput);
+			
+			
+			
+		} while(0 >= menuInput || menuInput > 4);
+		
+		
+	}
 
-    public Menu(String InputID) {
+	public int getMenuInput() {
+		return menuInput;
+	}
 
-        System.out.println(InputID + " 님 환영합니다 !\n\n");
-        String tempInput = null;
-
-        do {
-
-
-            System.out.println("메뉴 번호를 입력해 주세요.\n");
-
-            System.out.println("=========================");
-
-            System.out.println("1. 사용자 성향 분석 목록\n");
-            System.out.println("2. 사용자 대출 이력/반납 목록\n");
-            System.out.println("3. 추천 시스템 이용\n");
-            System.out.println("4. 어플리케이션 종료\n");
-
-            System.out.println(">> ");
-
-            Scanner sc = new Scanner(System.in);
-
-            tempInput = sc.next();
-
-            menuInput = Integer.parseInt(tempInput);
-
-
-
-        } while(0 >= menuInput || menuInput > 4);
-
-
-    }
-
-    public int getMenuInput() {
-        return menuInput;
-    }
-
-    public void setMenuInput(int menuInput) {
-        this.menuInput = menuInput;
-    }
-
-
+	public void setMenuInput(int menuInput) {
+		this.menuInput = menuInput;
+	}
+	
+	
 }
--- 2019OSS/src/com/github/cauoss2019/team03/UserPropensityAnalysis.java
+++ 2019OSS/src/com/github/cauoss2019/team03/UserPropensityAnalysis.java
@@ -1,3 +1,5 @@
+package com.github.cauoss2019.team03;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,96 +15,103 @@ import java.util.Scanner;
 601~700 : 자기계발*/
 
 public class UserPropensityAnalysis {
-
-    private final String genre1 = "자연과학";
-    final private String genre2 = "IT";
-    final private String genre3 = "에세이";
-    final private String genre4 = "역사";
-    final private String genre5 = "예술";
-    final private String genre6 = "인문";
-    final private String genre7 = "자기계발";
-
-    public UserPropensityAnalysis() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-    public UserPropensityAnalysis(String inputID) {
-
-        List<String> genre = new ArrayList<String>();
-
-        //String path = UserPropensityAnalysis.class.getResource("").getPath();
-
-        List<List<String>> records = new Read("resultData_ver4.csv", "euc-kr").getRecords();
-
-        for(int i=0; i<records.size(); i++) {
-
-            int bookId = Integer.parseInt(records.get(i).get(1));
-
-
-            if(bookId < 101) {
-                genre.add(genre1); // 자연과학
-            }
-            else if(bookId < 201) {
-                genre.add(genre2); // IT
-            }
-            else if(bookId < 301) {
-                genre.add(genre3); // 에세이
-            }
-            else if(bookId < 401) {
-                genre.add(genre4); // 역사
-            }
-            else if(bookId < 501) {
-                genre.add(genre5); // 예술
-            }
-            else if(bookId < 601) {
-                genre.add(genre6); // 인문
-            }
-            else if(bookId < 701){
-                genre.add(genre7); // 자기계발
-            }
-
-        }
-
-        Map<String, Integer> counts = new HashMap<String, Integer>();
-
-        for (String str : genre) {
-
-            if(counts.containsKey(str)) {
-                counts.put(str, counts.get(str)+1);
-
-            }
-            else {
-                counts.put(str, 1);
-            }
-
-        }
-
-        int sum = records.size(); // 전체 빌린 책의 수
-
-
-        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
-            System.out.println(entry.getKey() + " = " + String.format("%.2f", (double)(entry.getValue() / (double)sum) * 100.00) + " % ");
-
-        }
-        //index(4) >> 장르
-
-        System.out.println("======================\n\n");
-
-        System.out.println("메뉴로 돌아가려면 아무키나 누르세요.");
-
-        try {
-
-            System.in.read();
-
-            System.out.println("\n\n======================");
-
-        } catch(Exception e) {
-
-            e.printStackTrace();
-
-        }
-    }
-
-
+	
+	private final String genre1 = "자연과학";
+	final private String genre2 = "IT";
+	final private String genre3 = "에세이";
+	final private String genre4 = "역사";
+	final private String genre5 = "예술";
+	final private String genre6 = "인문";
+	final private String genre7 = "자기계발";
+	
+	public UserPropensityAnalysis() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserPropensityAnalysis(String inputID) {
+		
+		List<String> genre = new ArrayList<String>();
+		
+		String path = UserPropensityAnalysis.class.getResource("").getPath();
+		
+		List<List<String>> records = new Read(path + "resultData_ver4.csv", "euc-kr").getRecords();
+		
+		int sum = 0;
+		
+		for(int i=0; i<records.size(); i++) {
+			
+			if(records.get(i).get(0).equals(inputID)) {
+				
+				int bookId = Integer.parseInt(records.get(i).get(1));
+				
+				if(bookId < 101) {
+					genre.add(genre1); // 자연과학
+				}
+				else if(bookId < 201) {
+					genre.add(genre2); // IT
+				}
+				else if(bookId < 301) {
+					genre.add(genre3); // 에세이
+				}
+				else if(bookId < 401) {
+					genre.add(genre4); // 역사
+				}
+				else if(bookId < 501) {
+					genre.add(genre5); // 예술
+				}
+				else if(bookId < 601) {
+					genre.add(genre6); // 인문
+				}
+				else if(bookId < 701){
+					genre.add(genre7); // 자기계발
+				}
+				sum++;
+			}
+		
+		}
+		System.out.println("\n==========================");
+		System.out.println("* "+inputID+" 님은 총 "+sum+" 권을 빌리셨습니다.");
+		System.out.println("==========================\n");
+		
+		Map<String, Integer> counts = new HashMap<String, Integer>();
+		
+		for (String str : genre) {
+			
+			if(counts.containsKey(str)) {
+				counts.put(str, counts.get(str)+1);
+				
+			}
+			else {
+				counts.put(str, 1);
+			}
+			
+		}
+		
+		
+		for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+			System.out.println(entry.getKey() + "\t" + entry.getValue() + " 권 \t("
+					+ String.format("%.2f", (double)(entry.getValue() / (double)sum) * 100.00) + " %)");
+			
+		}
+		//index(4) >> 장르
+		
+		System.out.println("======================\n\n");
+		
+		System.out.println("메뉴로 돌아가려면 아무키나 누르세요.");
+		
+		try {
+			
+			System.in.read();
+			
+			System.out.println("\n\n======================");
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+	
+	
 }
